e79c78679f9c3e4f66fd294474d755c9fc2e0202
Racheli Viner <150911456+racheliviner@users.noreply.github.com>
2024-01-06 21:11:09 +0200

    Update Ball.java

--- src/bricker/gameobjects/Ball.java
+++ src/bricker/gameobjects/Ball.java
@@ -1,23 +1,37 @@
 package bricker.gameobjects;
 
+import bricker.brick_strategies.CollisionStrategy;
 import danogl.GameObject;
 import danogl.collisions.Collision;
-import danogl.gui.Sound;
 import danogl.gui.rendering.Renderable;
 import danogl.util.Vector2;
 
-public class Ball extends GameObject {
-    Sound collisionSound;
-    public Ball(Vector2 topLeftCorner, Vector2 dimensions, Renderable renderable, Sound collisionSound) {
+public class Brick extends GameObject {
+
+    // Strategy for handling collisions involving the Brick
+    private CollisionStrategy collisionStrategy;
+
+    // Constructor
+    public Brick(Vector2 topLeftCorner, Vector2 dimensions, Renderable renderable, CollisionStrategy collisionStrategy) {
+        // Call the superclass constructor
         super(topLeftCorner, dimensions, renderable);
-        this.collisionSound = collisionSound;
+
+        // Set the collision strategy for the Brick
+        this.collisionStrategy = collisionStrategy;
     }
+
+    /**
+     * Overrides the onCollisionEnter method to handle collision events.
+     *
+     * @param other     The other game object involved in the collision.
+     * @param collision Details of the collision.
+     */
     @Override
     public void onCollisionEnter(GameObject other, Collision collision) {
+        // Call the superclass method
         super.onCollisionEnter(other, collision);
-        if(! (other instanceof Heart)){
-            setVelocity(getVelocity().flipped(collision.getNormal()));
-            collisionSound.play();
-        }
+
+        // Delegate the collision handling to the associated CollisionStrategy
+        collisionStrategy.onCollision(this, other);
     }
 }
