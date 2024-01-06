65f48bf2fa0a5e3e32ca655a5bb16f0b5c7d2c6a
Racheli Viner <150911456+racheliviner@users.noreply.github.com>
2024-01-06 21:09:41 +0200

    Update CollisionStrategy.java

--- src/bricker/brick_strategies/CollisionStrategy.java
+++ src/bricker/brick_strategies/CollisionStrategy.java
@@ -1,31 +1,51 @@
 package bricker.brick_strategies;
 
 import bricker.gameobjects.Ball;
-import bricker.gameobjects.Brick;
 import danogl.GameObject;
 import danogl.collisions.GameObjectCollection;
-import danogl.collisions.Layer;
 import danogl.util.Vector2;
 
 public class CollisionStrategy {
 
+    // Collection of game objects
     private GameObjectCollection object;
+
+    // Number of remaining bricks
     private int numOfBricks;
 
+    // Constructor
     public CollisionStrategy(GameObjectCollection object, int numOfBricks) {
         this.object = object;
         this.numOfBricks = numOfBricks;
     }
 
+    /**
+     * Handles collision between game objects.
+     *
+     * @param thisObj   The current game object.
+     * @param otherObj  The other game object involved in the collision.
+     */
     public void onCollision(GameObject thisObj, GameObject otherObj) {
+        // Check if the collision involves a Ball object
         if (otherObj instanceof Ball) {
-            if(numOfBricks==1)
-                thisObj.setCenter(new Vector2(-50,-50));
+            // Special condition: Set the position of the object to (-50, -50)
+            if (numOfBricks == 1) {
+                thisObj.setCenter(new Vector2(-50, -50));
+            }
+
+            // Remove the collided brick from the object collection
             this.object.removeGameObject(thisObj);
+
+            // Decrement the number of remaining bricks
             numOfBricks--;
         }
     }
 
+    /**
+     * Gets the number of remaining bricks.
+     *
+     * @return The number of remaining bricks.
+     */
     public int getNumOfBricks() {
         return numOfBricks;
     }
