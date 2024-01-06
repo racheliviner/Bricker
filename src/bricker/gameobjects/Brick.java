5fdbe5233532fc16b3a67206a06337433e21c701
Racheli Viner <150911456+racheliviner@users.noreply.github.com>
2024-01-06 21:11:45 +0200

    Update Brick.java

--- src/bricker/gameobjects/Brick.java
+++ src/bricker/gameobjects/Brick.java
@@ -3,22 +3,35 @@ package bricker.gameobjects;
 import bricker.brick_strategies.CollisionStrategy;
 import danogl.GameObject;
 import danogl.collisions.Collision;
-import danogl.collisions.GameObjectCollection;
 import danogl.gui.rendering.Renderable;
 import danogl.util.Vector2;
 
 public class Brick extends GameObject {
+
+    // Strategy for handling collisions involving the Brick
     private CollisionStrategy collisionStrategy;
 
-
+    // Constructor
     public Brick(Vector2 topLeftCorner, Vector2 dimensions, Renderable renderable, CollisionStrategy collisionStrategy) {
+        // Call the superclass constructor
         super(topLeftCorner, dimensions, renderable);
+
+        // Set the collision strategy for the Brick
         this.collisionStrategy = collisionStrategy;
     }
 
+    /**
+     * Overrides the onCollisionEnter method to handle collision events.
+     *
+     * @param other     The other game object involved in the collision.
+     * @param collision Details of the collision.
+     */
     @Override
     public void onCollisionEnter(GameObject other, Collision collision) {
+        // Call the superclass method
         super.onCollisionEnter(other, collision);
+
+        // Delegate the collision handling to the associated CollisionStrategy
         collisionStrategy.onCollision(this, other);
     }
 }
